37dfe0b16fbf407675de59c5f8eecec5fc873121
180485 <ismiandhykawaty@icloud.com>
2020-11-12 16:10:52 +0100

    exercise date 03

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -9,8 +9,25 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // your code here
 
-})();
+    
+     document.getElementById("run").addEventListener("click", function() {
+    
+    var date2 = new Date ();
+    
+    var birthday = new Date (document.getElementById("dob-year").value ,document.getElementById("dob-month").value,document.getElementById("dob-day").value);
+    
+    var age = date2.getFullYear()- birthday.getFullYear();
+     
+    var months = date2.getMonth()-birthday.getMonth();
+     if (months < 0 || (months === 0 && datumVandaag.getDate() < birthday.getDate())){
+        age --;
+    } 
+       
+     document.getElementById("post").innerHTML = " <h2 style=\"text-align: center;\"> your age is: " + age + " Years old!! </h2>";
+
+}
+
+
+    )
