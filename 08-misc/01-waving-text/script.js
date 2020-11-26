2ca435851dfbc7db842db1bf9eaf1b80c981854f
180485 <ismiandhykawaty@icloud.com>
2020-11-26 13:34:44 +0100

    01-waving text exercise complete

--- 08-misc/01-waving-text/script.js
+++ 08-misc/01-waving-text/script.js
@@ -10,7 +10,38 @@
 // You will have time to focus on it later.
 
 (function() {
+    (function() {
+        
+        var letterContainer = document.getElementById("target");
+    
+        var letters = letterContainer.innerText;
+    
+        console.log(letters);
+    
+        var lettersArray = letters.split(" ");
+    
+        console.log(lettersArray);
+    
+        letterContainer.innerText = "";
+    
+    
+      //  var result = lettersArray[0].fontsize(5);
+    
+        var i;
+        for (i = 0; i < lettersArray.length; i++) {
+            var value = getRandomInt(10);
+            var result = lettersArray[i].fontsize(value);
+          
+    
+            document.getElementById("target").innerHTML += result;
+        }
+    
+    
+         function getRandomInt(max) {
+             return Math.floor(Math.random() * Math.floor(max));
+         }
+    
+    
 
-    // your code here
-
+    })();
 })();
