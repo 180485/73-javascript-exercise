ca18fd7719bde78951dd8eb04675f66e072f85bc
180485 <ismiandhykawaty@icloud.com>
2020-11-26 20:17:39 +0100

    exercise 03-misc complete

--- 08-misc/03-worst-ui-one/script.js
+++ 08-misc/03-worst-ui-one/script.js
@@ -11,6 +11,13 @@
 
 (function() {
 
-    // your code here
+let slider = document.getElementById("slider");
+let output = document.getElementById("target");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+
+}
 
 })();
